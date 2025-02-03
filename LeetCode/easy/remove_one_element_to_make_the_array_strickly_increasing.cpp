87e52a01a59261674f68bf3c18843edff019bac9
HoonSooni <yimsh0530@gmail.com>
2025-02-03 12:10:12 +0900

    Additional solution for the problem 1909 has bee added

--- LeetCode/easy/remove_one_element_to_make_the_array_strickly_increasing.cpp
+++ LeetCode/easy/remove_one_element_to_make_the_array_strickly_increasing.cpp
@@ -11,7 +11,7 @@
 #include <unordered_set>
 
 using namespace std;
-
+/*
 class Solution {
 public:
     void print(vector<int>& nums) {
@@ -43,6 +43,26 @@ public:
         }
 
         return true;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.size() -1; ++i) {
+            if (nums[i] >= nums[i + 1]) {
+                count++;
+                
+                if (i > 0 && nums[i - 1] >= nums[i + 1]) {
+                    nums[i + 1] = nums[i];
+                }
+            }
+        }
+        
+        return count > 2;
     }
 };
 
